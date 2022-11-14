cf8d3c7aa52281d90fdc390c842e173c199e3e09
alanzarate <alnzarate@gmail.com>
2022-11-13 21:42:43 -0400

    first commit

---
+++ src/main/java/bo/ucb/edu/backendSpringMrJeff/BackendSpringMrJeffApplication.java
@@ -0,0 +1,13 @@
+package bo.ucb.edu.backendSpringMrJeff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringMrJeffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringMrJeffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/ucb/edu/backendSpringMrJeff/BackendSpringMrJeffApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.ucb.edu.backendSpringMrJeff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringMrJeffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
